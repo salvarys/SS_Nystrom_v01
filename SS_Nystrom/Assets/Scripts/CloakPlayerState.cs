732bbc9a6aadff69d255098329be1c81c11a1813
salvarys <salvarys@my.erau.edu>
2024-12-07 14:25:59 -0700

    Update Commands

--- SS_Nystrom/Assets/Scripts/CloakPlayerState.cs
+++ SS_Nystrom/Assets/Scripts/CloakPlayerState.cs
@@ -1,27 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CloakPlayerState : IPlayerState
 {
-    Player mPlayer;
-    Rigidbody rbPlayer;
+    private Player mPlayer;
+    private Renderer playerRenderer; // Used to control visibility
+    private bool isCloaked;          // Tracks if the player is currently cloaked
 
     public void Enter(Player player)
     {
         Debug.Log("Cloak On");
-        rbPlayer = player.GetComponent<Rigidbody>();
+        mPlayer = player;
 
-        player.mCurrentState = this;
+        // Get the Renderer component for visibility control
+        playerRenderer = player.GetComponent<Renderer>();
+
+        // Ensure player starts visible
+        playerRenderer.enabled = true;
+        isCloaked = false;
+
+        player.mCurrentState = this; // Update player's state
     }
-
 
     public void Execute(Player player)
     {
-        if (Input.GetKey(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C)) // Toggle visibility on key press
         {
-            StandingPlayerState standingState = new StandingPlayerState();
-            standingState.Enter(player);
+            if (isCloaked)
+            {
+                // Turn visible
+                playerRenderer.enabled = true;
+                isCloaked = false;
+                Debug.Log("Cloak Off - Player is now visible.");
+                StandingPlayerState standingState = new StandingPlayerState();
+                standingState.Enter(player);
+            }
+            else
+            {
+                // Turn invisible
+                playerRenderer.enabled = false;
+                isCloaked = true;
+                Debug.Log("Cloak On - Player is now invisible.");
+            }
+
+    
         }
     }
 }
